f1eb2a8edeeab56bc6be4dde0d6554b4d2270909
mahmoodhamdi <mahmouashwah@gmail.com>
2024-01-24 10:59:04 +0200

    database conected

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 dotenv.config();
+mongoose.connect(process.env.MONGOURI)
+    .then(() => console.log("Foodly Database connected"))
+    .catch((err) => console.log(err));
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(process.env.PORT, () => console.log(`Foodly app listening on port ${process.env.PORT}!`))
+app.listen(process.env.PORT, () => console.log(`Foodly app listening on port ${process.env.PORT}!`))    
