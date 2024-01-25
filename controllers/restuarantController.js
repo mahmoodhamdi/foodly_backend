0c95bc069c9fcc1727dfec62bea43465baa733f0
mahmoodhamdi <mahmouashwah@gmail.com>
2024-01-25 08:03:51 +0200

    finish restuarant controller

--- controllers/restuarantController.js
+++ controllers/restuarantController.js
@@ -1,35 +1,85 @@
-const Category = require('../models/category');
+const restuarant = require('../models/restuarant');
+const Restuarant = require('../models/restuarant');
 module.exports = {
-    CreateCategory: async (req, res) => {
-        const newCategory = new Category(req.body);
+    addRestuarant: async (req, res) => {
+        const { title, value, imageUrl, coords, foods, pickup, delivery } = req.body;
+        if (!title || !value || !imageUrl || !coords || !foods || !pickup || !delivery || !coords.latitude || !coords.longitude) {
+            return res.status(400).json({ message: 'All fields are required', success: false });
+
+        }
+        const newRestuarant = new Restuarant(req.body);
         try {
-            await newCategory.save();
-            res.status(201).json({ message: 'Category created successfully', category: newCategory, success: true });
+            await newRestuarant.save();
+            res.status(201).json({ message: 'Restuarant created successfully', success: true });
 
         } catch (err) {
             res.status(500).json({ message: err.message, success: false });
         }
     },
-    GetAllCategories: async (req, res) => {
-        try {
-            const categories = await Category.find({ title: { $ne: 'More' } }, { __v: 0 });
-            res.status(200).json({ message: 'Categories fetched successfully', categories: categories, success: true });
-        } catch (err) {
-            res.status(500).json({ message: err.message, success: false });
-        }
-    },
-    GetRandomCategories: async (req, res) => {
-        try {
-            let categories = await Category.aggregate([
-                { $match: { value: { $ne: 'more' } } },
-                { $sample: { size: 4 } },
+    GetRandomRestuarants: async (req, res) => {
+        const code = req.params.code;
 
-            ]);
-            const moreCategory = await Category.findOne({ value: 'more' }, { __v: 0 });
-            if (moreCategory) {
-                categories.push(moreCategory);
+        try {
+            let randomRestuarants = [];
+            if (code) {
+                randomRestuarants = await Restuarant.aggregate([
+                    { $match: { code: code, isavailable: true } },
+
+                    {
+                        $sample: {
+                            size: 5
+                        }
+                    },
+
+                    { $match: { project: { __v: 0 } } },
+                ]);
             }
-            res.status(200).json({ message: 'Categories fetched successfully', categories: categories, success: true });
+            if (randomRestuarants.length === 0) {
+                randomRestuarants = await Restuarant.aggregate([
+                    { $match: { code: code, isavailable: true } },
+                ]);
+
+            }
+
+
+
+            res.status(200).json({ message: 'Restuarants fetched successfully', restuarants: randomRestuarants, success: true });
+        } catch (err) {
+            res.status(500).json({ message: err.message, success: false });
+        }
+    }, GetRestaurantById: async (req, res) => {
+        const restuarantId = req.params.id;
+        try {
+            const restuarant = await Restuarant.findOne({ _id: restuarantId }, { __v: 0 });
+            res.status(200).json({ message: 'Restuarants fetched successfully', categories: categories, success: true });
+        } catch (err) {
+            res.status(500).json({ message: err.message, success: false });
+        }
+    },
+    GetAllNearbyRestuarants: async (req, res) => {
+        const code = req.params.code;
+
+        try {
+            let allRestuarants = [];
+            if (code) {
+                allRestuarants = await Restuarant.aggregate([
+                    { $match: { code: code, isavailable: true } },
+
+
+
+                    { $match: { project: { __v: 0 } } },
+                ]);
+            }
+            if (allRestuarants.length === 0) {
+                allRestuarants = await Restuarant.aggregate([
+                    { $match: { code: code, isavailable: true } },
+                ]);
+
+            }
+
+
+
+            res.status(200).json({ message: 'Restuarants fetched successfully', restuarants: allRestuarants, success: true });
         } catch (err) {
             res.status(500).json({ message: err.message, success: false });
         }
