e4ad52d7eb4018a795b9d4036c70d16e139d2772
mahmoodhamdi <mahmouashwah@gmail.com>
2024-02-02 09:11:07 +0200

    address model

---
+++ models/address.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const addressSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+
+    },
+    addressLine1: {
+        type: String,
+        required: true
+
+    },
+    postalCode: {
+        type: String,
+        required: true
+
+    },
+    default: {
+        type: Boolean,
+        default: false
+    },
+    deliveryInstructions: {
+        type: String,
+        required: false
+    },
+    latitude: {
+        type: Number,
+        required: false
+    },
+    longitude: {
+        type: Number,
+        required: false
+    }
+});
+module.exports = mongoose.model('Address', addressSchema)
